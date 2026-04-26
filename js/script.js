f2388d297b1fe64ad3439f04d795853ae4f761a9
Noemi Muñiz Gamero <noemipracticasdaw@gmail.com>
2026-04-26 18:00:11 +0200

    Recuperadas secciones libro y personal

--- js/script.js
+++ js/script.js
@@ -12,14 +12,3 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector(".header");
   header.classList.toggle("scrolled", window.scrollY > 10);
 });
-
-/** BOTON PARA MOSTAR LOS ENLACES DEL LIBRO */
-function mostrarTiendas(){
-  const tiendas = document.getElementById("tiendas-libro");
-
-  if(tiendas.style.display === "block"){
-    tiendas.style.display = "none";
-  }else{
-    tiendas.style.display = "block";
-  }
-}
